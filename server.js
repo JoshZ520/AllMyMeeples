fc6356f3565f5b2b9acdfa20c9b30a21cc27b2e6
Josh <joshuazob@gmail.com>
2026-04-08 11:19:46 -0600

    added 0ip address

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 async function startServer() {
   try {
     await initDb();
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server running on http://localhost:${PORT}`);
     });
   } catch (error) {
